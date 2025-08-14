51262da4b85e6548407d61a2084349dd8d1caf8e
Justin Grote <JustinGrote@users.noreply.github.com>
2025-08-14 16:33:37 -0700

    Disable content example for now

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -1,24 +1,20 @@
-import { MantineProvider, createTheme } from '@mantine/core'
 import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './views/App.tsx'
 
-console.log('[CRXJS] Hello world from content script!')
+// console.log('[CRXJS] Hello world from content script!')
 
-const theme = createTheme({
-	// You can customize the theme here
-	primaryColor: 'blue',
-})
+// const theme = createTheme({
+// 	// You can customize the theme here
+// 	primaryColor: 'blue',
+// })
 
-const container = document.createElement('div')
-container.id = 'crxjs-app'
-document.body.appendChild(container)
-createRoot(container).render(
-	<StrictMode>
-		<MantineProvider theme={theme}>
-			<App />
-		</MantineProvider>
-	</StrictMode>,
-)
+// const container = document.createElement('div')
+// container.id = 'crxjs-app'
+// document.body.appendChild(container)
+// createRoot(container).render(
+// 	<StrictMode>
+// 		<MantineProvider theme={theme}>
+// 			<App />
+// 		</MantineProvider>
+// 	</StrictMode>,
+// )
