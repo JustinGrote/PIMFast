6ae47f6cd6e4870364d6b268f5625f92247d842c
Justin Grote <JustinGrote@users.noreply.github.com>
2025-09-04 16:23:45 -0700

    Convert to record from map

--- src/api/graphClient.ts
+++ src/api/graphClient.ts
@@ -3,19 +3,19 @@ import { Client } from '@microsoft/microsoft-graph-client'
 import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials'
 import { AccountInfoHomeId, AccountInfoTokenCredential } from './auth'
 
-const graphClients: Map<AccountInfoHomeId, Client> = new Map()
+const graphClients: Record<AccountInfoHomeId, Client> = {}
 
 /** Returns a singleton global client, per best practice */
 export async function getGraphClient(account: AccountInfo): Promise<Client> {
 	const cacheKey = account.homeAccountId
-	let client: Client | undefined = graphClients.get(cacheKey)
+	let client: Client | undefined = graphClients[cacheKey]
 	if (!client) {
 		client = Client.initWithMiddleware({
 			authProvider: new TokenCredentialAuthenticationProvider(new AccountInfoTokenCredential(account), {
 				scopes: ['https://graph.microsoft.com/.default'],
 			}),
 		})
-		graphClients.set(cacheKey, client)
+		graphClients[cacheKey] = client
 	}
 	return client
 }
